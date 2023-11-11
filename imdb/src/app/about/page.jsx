a6170a2360622741a4893fe0f52fef65b05d6e18
jklip506 <96444138+jklip506@users.noreply.github.com>
2023-11-10 22:48:54 -0600

    added about page

---
+++ imdb/src/app/about/page.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to MovieDB, the ultimate online destination for movie lovers.
+        MovieDB is a website that lets you discover, rate, and review movies
+        from all over the world. Whether you are looking for the latest
+        blockbusters, the classics, or the hidden gems, MovieDB has it all. You
+        can browse movies by genre, year, country, language, or popularity. You
+        can also search for movies by title, director, actor, or keyword.
+      </p>
+      <p>
+        MovieDB provides you with detailed information about each movie, such as
+        the synopsis, cast, crew, trivia, awards, ratings, and reviews. You can
+        also watch trailers, clips, and interviews from your favorite movies.
+        MovieDB is more than just a website, it is a community of movie
+        enthusiasts. You can create your own profile, rate and review movies,
+        create and join lists, follow and interact with other users, and get
+        personalized recommendations based on your preferences.
+      </p>
+      <p>
+        You can also participate in polls, quizzes, contests, and forums to
+        share your opinions and insights with other movie fans. MovieDB is
+        constantly updated with new and exciting features to enhance your movie
+        experience. MovieDB is the ultimate movie database for movie lovers.
+        Join MovieDB today and discover a world of movies at your fingertips.
+      </p>
+    </div>
+  );
+};
+
+export default page;
