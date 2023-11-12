674f123fbc745b13f899abaa3063f8336643de4a
jklip506 <96444138+jklip506@users.noreply.github.com>
2023-11-11 20:15:05 -0600

    created navbar component

--- imdb/src/app/about/page.jsx
+++ imdb/src/app/about/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="max-w-6xl mx-auto space-y-4">
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
       <h1 className="text-2xl font-medium text-amber-600">About</h1>
       <p>
         Welcome to MovieDB, the ultimate online destination for movie lovers.
