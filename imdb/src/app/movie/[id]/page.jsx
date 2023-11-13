01eea5b0330f1a3c15df0d442829e000fcbbe401
jklip506 <96444138+jklip506@users.noreply.github.com>
2023-11-12 20:32:15 -0600

    Added search ability

--- imdb/src/app/movie/[id]/page.jsx
+++ imdb/src/app/movie/[id]/page.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import Results from "@/components/Results";
 
 async function getMovie(movieId) {
   const res = await fetch(
@@ -8,30 +9,35 @@ async function getMovie(movieId) {
   return await res.json();
 }
 
+async function getSimilarMovies(movieId) {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=${process.env.API_KEY}`
+  );
+  return await res.json();
+}
+
 const MoviePage = async ({ params }) => {
   const movieId = params.id;
   const movie = await getMovie(movieId);
+  const similarMovies = await getSimilarMovies(movieId);
+  const similarMoviesResults = similarMovies.results.slice(0, 5);
+
   return (
-    <div className="w-full">
-      <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
-        <Image
-          src={`https://image.tmdb.org/t/p/original/${
-            movie.backdrop_path || movie.poster_path
-          }`}
-          width={500}
-          height={300}
-          className="rounded-lg"
-          placeholder="blur"
-          blurDataURL="/spinner.svg"
-          alt="Movie poster"
-          style={{
-            maxWidth: "100%",
-            height: "100%",
-          }}
-        />
-        <div className="p-2">
-          <h2 className="text-lg mb-3 font-bold">{movie.title || movie.name}</h2>
-          <p className="text-lg mb-3">
+    <div className="flex flex-col min-h-screen w-full">
+      <div className="flex-grow">
+        <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
+          <Image
+            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`}
+            width={500}
+            height={300}
+            className="rounded-lg"
+            placeholder="blur"
+            blurDataURL="/spinner.svg"
+            alt="Movie poster"
+          />
+          <div className="p-2">
+            <h2 className="text-lg mb-3 font-bold underline underline-offset-8 rounded-lg">{movie.title || movie.name}</h2>
+            <p className="text-lg mb-3">
             <span className="font-semibold mr-1">Overview:</span>
             {movie.overview}
           </p>
@@ -43,7 +49,22 @@ const MoviePage = async ({ params }) => {
             <span className="font-semibold mr-1">Rating:</span>
             {movie.vote_count}
           </p>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Runtime:</span>
+            {movie.runtime} minutes
+          </p>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Genre(s)</span>
+            {movie.genres.map((genre) => genre.name).join(", ")}
+          </p>
+          </div>
         </div>
+      </div>
+
+      {/* Similar Movies Footer */}
+      <div className="w-full p-4 md:pt-8 mx-auto dark:bg-gray-600 bg-amber-100">
+        <h2 className="text-lg mb-3 font-bold text-center">Similar Movies</h2>
+        <Results results={similarMoviesResults} />
       </div>
     </div>
   );
---
+++ imdb/src/components/SearchBox.jsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const SearchBox = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const router = useRouter();
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!searchTerm) return;
+    router.push(`/search/${searchTerm}`);
+  }
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex max-w-6xl mx-auto justify-between items-center px-5"
+    >
+      <input
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        type="text"
+        placeholder="Search for a movie"
+        className="w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1"
+      />
+      <button disabled={!searchTerm} type="submit" className="text-amber-600 disabled:text-gray-400">
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default SearchBox;
